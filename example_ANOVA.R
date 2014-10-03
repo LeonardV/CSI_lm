86589a4068da637c960a05e1f7b27b59a32d3735
Leonard Vanbrabant <lgf.vanbrabant@gmail.com>
2014-10-03 16:11:09 +0200

    Create example_ANOVA.R

---
+++ example_ANOVA.R
@@ -0,0 +1,19 @@
+data <- read.csv("depression.csv")
+model <- 'depression ~ factor(group) -1'
+R1 <- rbind(c(-1,1,0), c(0,-1,1))
+
+## classical F test ##
+fit.lm <- lm(model, data=data)
+f <- summary(fit.lm)$fstatistic
+# F statistic
+f[1]
+# p value
+pf(f[1],f[2],f[3], lower.tail = FALSE)
+
+
+## F-bar test ##
+fit.csi <- csi.lm(model, data, ui = R1, mix.weights = "boot")
+# F-bar statistics
+fit.csi$T.obs
+# p values
+fit.csi$p.value
